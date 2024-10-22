bfb2038145eec0b53b299a0b3bd28b0f045f62d0
Sonnyy17 <SonNHSE173270@fpt.edu.vn>
2024-10-22 14:22:38 +0700

    UPDATE

---
+++ Application/Interfaces/IVNPayService.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs.VNPayDTO;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IVNPayService
+    {
+        Task<string> CreatePaymentUrl(HttpContext context, VnPaymentRequestModel model);
+        Task<VnPaymentResponseModel> PaymentExcute(IQueryCollection collections);
+    }
+}
